a7516312b0f1f40f781ac882864a253d492e0982
Ning Sun <sunng@about.me>
2015-11-10 22:39:13 +0800

    (feat) implement tick action

--- src/rigui/core.clj
+++ src/rigui/core.clj
@@ -14,16 +14,44 @@
 (defn create-timing-wheels [tick ^TimeUnit unit bucket-count consumer]
   (TimingWheels. (ref []) (.toNanos unit tick) bucket-count consumer))
 
-(defn- dump-task [parent wheel-level]
-  ;;TODO
-  )
+(defn- rotate [buckets]
+  (into [] (concat (rest buckets) [(first buckets)])))
+
+(defn- tick-action [parent wheel-level]
+  (let [wheel (nth @(.wheels parent) wheel-level)
+        bucket (first @(.buckets wheel))]
+
+    (if (= wheel-level 0)
+      (do
+        (doseq [t @bucket]
+          ((.consumer parent) (.task t)))
+        (dosync
+         (ref-set bucket #{})
+         (alter (.buckets wheel) rotate)))
+      (dosync
+       (doseq [t @bucket]
+         (let [d (.delay t)
+               next-wheel-delay (mod d (* (.tick parent)
+                                          (Math/pow (.bucket-count parent)
+                                                    wheel-level)))
+               next-bucket-index (quot next-wheel-delay
+                                       (* (.tick parent)
+                                          (Math/pow (.bucket-count parent)
+                                                    (dec wheel-level))))
+               next-wheel (nth @(.wheels parent) (dec wheel-level))]
+           (alter (nth @(.buckets next-wheel) next-bucket-index) conj t)))
+       ;; rotate buckets
+       (ref-set bucket #{})
+       (alter (.buckets wheel) rotate)))))
 
 (defn- create-wheel [parent level]
-  (let [buckets (repeat (.bucket-count parent) (ref #{}))
+  (let [buckets (ref (map (fn [_] (ref #{})) (range (.bucket-count parent))))
         schedule-future (agent nil)]
-    (send schedule-future #(.scheduleWithFixedDelay ^ScheduledExecutorService wheel-scheduler
-                                                     (partial dump-task parent level)
-                                                     (.tick parent) TimeUnit/NANOSECONDS))
+    (send schedule-future (fn [_] (.scheduleWithFixedDelay ^ScheduledExecutorService wheel-scheduler
+                                                          (partial tick-action parent level)
+                                                          0
+                                                          (* (.tick parent) (Math/pow (.bucket-count parent) level))
+                                                          TimeUnit/NANOSECONDS)))
     (TimingWheel. schedule-future buckets)))
 
 (defn level-and-bucket-for-delay [delay tick bucket-count]
@@ -32,16 +60,18 @@
     [level bucket]))
 
 (defn schedule! [tw task delay ^TimeUnit unit]
-  (let [[level bucket] (level-and-bucket-for-delay delay (.tick tw) (.bucket-count tw))]
+  (let [delay (.toNanos unit delay)
+        [level bucket] (level-and-bucket-for-delay delay (.tick tw) (.bucket-count tw))]
     (if (< level 0)
       ((.consumer tw) task)
       (let [task-entity (Task. task delay)]
         (dosync
          (let [wheels (alter (.wheels tw) (fn [wheels]
-                                            (let [current-wheel-count (count wheels)]
-                                              (if (> (- level current-wheel-count) 0)
-                                                (concat wheels (map #(create-wheel tw %)
-                                                                    (range current-wheel-count (inc level))))
+                                            (let [current-wheel-count (count wheels)
+                                                  levels-required (inc level)]
+                                              (if (> levels-required current-wheel-count)
+                                                (into [] (concat wheels (map #(create-wheel tw %)
+                                                                             (range current-wheel-count levels-required))))
                                                 wheels))))
                the-wheel (nth wheels level)]
-           (alter (nth (.buckets the-wheel) bucket) conj task-entity)))))))
+           (alter (nth @(.buckets the-wheel) bucket) conj task-entity)))))))

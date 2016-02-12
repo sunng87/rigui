5fdbab41c80b06d2c24f897800495451271f51d2
Ning Sun <sunng@about.me>
2016-02-12 12:07:46 +0800

    (chore) added clojars repo

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/sunng87/rigui"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :deploy-repositories {"releases" :clojars})

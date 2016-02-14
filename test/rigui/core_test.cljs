891d70e5fd16549ab23d7c2097751e341bfe02ec
Ning Sun <sunng@about.me>
2016-02-14 21:38:11 +0800

    (test) improve tests

--- test/rigui/core_test.cljs
+++ test/rigui/core_test.cljs
@@ -1,20 +1,19 @@
 (ns rigui.core-test
-  (:require [clojure.test :refer :all]
-            [rigui.core :refer :all])
-  (:import [java.util.concurrent Executors TimeUnit]))
+  (:require [cljs.test :refer-macros [deftest is testing]]
+            [rigui.core :refer :all]))
 
 (deftest test-scheduler
   (let [task-count 10000
         task-time 2500
         task-counter (atom task-count)
-        executor (Executors/newFixedThreadPool (.availableProcessors (Runtime/getRuntime)))
-        tws (start 1 8 (fn [_] (swap! task-counter dec)) executor)]
+        tws (start 1 8 (fn [_] (swap! task-counter dec)))
+        verifier-tws (start 1 12 (fn [_]
+                                   (is (= (count (stop tws) 0)))
+                                   (is (= @task-counter 0))))]
     (time
      (dotimes [_ task-count]
        (schedule! tws nil (rand-int task-time))))
-    (Thread/sleep (* 1.1 task-time))
-    (is (= @task-counter 0))
-    (is (= (count (stop tws)) 0))))
+    (schedule! verifier-tws nil task-time)))
 
 (deftest test-cancel
   (let [tw (start 1 8 (fn [_] (is false)))

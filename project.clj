d17a34f0b2c2192aaa58d2d568bf50c8fe6e186c
Ning Sun <sunng@about.me>
2016-02-16 16:22:57 +0800

    (chore) bump version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject rigui "0.4.1-SNAPSHOT"
+(defproject rigui "0.5.0-SNAPSHOT"
   :description "Timing Wheels"
   :url "https://github.com/sunng87/rigui"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.7.228" :scope "provided"]]
   :plugins [[lein-cljsbuild "1.1.2"]]
   :cljsbuild {:builds [{:source-paths ["src"]

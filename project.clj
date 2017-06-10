de4d38bad937d756308d1355e4fe51fcb4489b42
Ning Sun <sunng@about.me>
2017-06-10 22:51:39 +0800

    (feat) update dependencies
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
-                 [org.clojure/core.async "0.2.374" :scope "provided"]]
-  :plugins [[lein-cljsbuild "1.1.2"]
-            [lein-doo "0.1.6"]]
+                 [org.clojure/clojurescript "1.9.562" :scope "provided"]
+                 [org.clojure/core.async "0.3.443" :scope "provided"]]
+  :plugins [[lein-cljsbuild "1.1.6"]
+            [lein-doo "0.1.7"]]
   :cljsbuild {:builds [{:id "core"
                         :source-paths ["src"]
                         :compiler {:optimizations :advanced
